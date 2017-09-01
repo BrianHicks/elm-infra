2c4fc27f8b35a3ab78b72c86ae4672987c5350e8
Brian Hicks <brian@brianthicks.com>
2017-09-01 17:09:34 -0500

    packer: add htop

--- packer/base.sh
+++ packer/base.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 # base dependencies
 apt-get update
-apt-get install --yes apt-transport-https ca-certificates curl software-properties-common
+apt-get install --yes apt-transport-https ca-certificates curl software-properties-common htop
 
 apt-get upgrade --yes
