1d440d71d55f457f355da9f946e84c1527e90cf6
Brian Hicks <brian@brianthicks.com>
2017-09-01 17:25:46 -0500

    packer: add rexray

---
+++ packer/rexray.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# install rexray
+curl -sSL https://dl.bintray.com/emccode/rexray/install | sh
+
+# disable rexray system service (we install it after creating an instance via Terraform)
+systemctl disable rexray
+
+# create wrapper script
+cat > /usr/local/bin/rexray <<EOF
+#!/usr/bin/env bash
+set -euo pipefail
+
+export REXRAY_SERVICE="dobs"
+export DOBS_REGION="\$(curl http://169.254.169.254/metadata/v1/region)"
+
+if test -z "\$DOBS_TOKEN"; then echo '"DOBS_TOKEN" is not set, but is required'; exit 1; fi
+
+exec /usr/bin/rexray "\$@"
+EOF
+chmod +x /usr/local/bin/rexray
