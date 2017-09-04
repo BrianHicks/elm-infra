26f1e7000e4df6fa17f2dee797a31dc18621c312
Brian Hicks <brian@brianthicks.com>
2017-09-04 14:43:04 -0500

    packer: noninteractive DEBIAN_FRONTEND

--- packer/base.sh
+++ packer/base.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export DEBIAN_FRONTEND=noninteractive
+
 # base dependencies
 apt-get update
 apt-get install --yes apt-transport-https ca-certificates curl software-properties-common htop
 
-apt-get upgrade --yes
+apt-get upgrade --yes --quiet
