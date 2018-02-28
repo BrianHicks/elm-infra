118f886beee05a3cea00d51d6a5edad32ec68f57
Brian Hicks <brian@brianthicks.com>
2018-02-28 07:53:35 -0800

    move tag and firewall to a group tag

--- infra/digitalocean.tf
+++ infra/digitalocean.tf
@@ -1,10 +1,14 @@
-variable "rexray_token" {}
+# variable "rexray_token" {}
 
 variable "region" {
   default = "nyc1"
 }
 
-provider "digitalocean" {}
+variable "digitalocean_token" {}
+
+provider "digitalocean" {
+  token = "${var.digitalocean_token}"
+}
 
 resource "digitalocean_ssh_key" "local" {
   name       = "local from elm-infra"
@@ -13,99 +17,13 @@ resource "digitalocean_ssh_key" "local" {
 
 /* MANAGERS */
 
-resource "digitalocean_tag" "elm-manager" {
-  # including this to future-proof. If we ever have followers, we should add a
-  # related tag and use these to restrict traffic via the firewalls.
-  name = "elm-manager"
-}
 
-module "manager-1-20170828-1" {
-  source       = "manager"
-  image_name   = "elm-infra-base_20170828-1"
-  name         = "manager-1-20170828-1"
-  tag          = "${digitalocean_tag.elm-manager.name}"
-  key_id       = "${digitalocean_ssh_key.local.id}"
-  region       = "${var.region}"
-  rexray_token = "${var.rexray_token}"
-}
-
-resource "digitalocean_firewall" "elm-manager" {
-  name = "elm-manager"
-  tags = ["${digitalocean_tag.elm-manager.name}"]
-
-  # these are in a wacky order because the DigitalOcean API considers these a
-  # set and returns them in whatever order it likes. Terraform, on the other
-  # hand, assumes that they're as specified by the last run. So we just need to
-  # set our source order to the same implementation order of the API so we don't
-  # get weird diffs.
-  #
-  # TODO: follow https://github.com/terraform-providers/terraform-provider-digitalocean/issues/30 and upgrade when it's fixed.
-  inbound_rule = [
-    {
-      # SSH
-      protocol         = "tcp"
-      port_range       = "22"
-      source_addresses = ["0.0.0.0/0", "::/0"]
-
-      # TODO: lock down access with source_address?
-    },
-    {
-      # HTTP
-      protocol         = "tcp"
-      port_range       = "80"
-      source_addresses = ["0.0.0.0/0", "::/0"]
-    },
-    {
-      # HTTPS
-      protocol         = "tcp"
-      port_range       = "443"
-      source_addresses = ["0.0.0.0/0", "::/0"]
-    },
-    {
-      # ping
-      protocol         = "icmp"
-      source_addresses = ["0.0.0.0/0", "::/0"]
-    },
-    {
-      # docker manager chit-chat
-      protocol    = "tcp"
-      port_range  = "2377"
-      source_tags = ["${digitalocean_tag.elm-manager.name}"]
-    },
-    {
-      # docker mesh discovery
-      protocol    = "tcp"
-      port_range  = "7946"
-      source_tags = ["${digitalocean_tag.elm-manager.name}"]
-    },
-    {
-      # docker mesh discovery
-      protocol    = "udp"
-      port_range  = "7946"
-      source_tags = ["${digitalocean_tag.elm-manager.name}"]
-    },
-    {
-      # docker ingress network
-      protocol    = "udp"
-      port_range  = "4789"
-      source_tags = ["${digitalocean_tag.elm-manager.name}"]
-    },
-  ]
-
-  outbound_rule = [
-    {
-      protocol              = "icmp"
-      destination_addresses = ["0.0.0.0/0", "::/0"]
-    },
-    {
-      protocol              = "tcp"
-      port_range            = "all"
-      destination_addresses = ["0.0.0.0/0", "::/0"]
-    },
-    {
-      protocol              = "udp"
-      port_range            = "all"
-      destination_addresses = ["0.0.0.0/0", "::/0"]
-    },
-  ]
-}
+# module "manager-1-20170828-1" {
+#   source       = "manager"
+#   image_name   = "elm-infra-base_20170828-1"
+#   name         = "manager-1-20170828-1"
+#   tag          = "${digitalocean_tag.elm-manager.name}"
+#   key_id       = "${digitalocean_ssh_key.local.id}"
+#   region       = "${var.region}"
+#   rexray_token = "${var.rexray_token}"
+# }
---
+++ infra/leader-group.tf
@@ -0,0 +1,51 @@
+resource "digitalocean_tag" "elm-leader" {
+  # including this to future-proof. If we ever have followers, we should add a
+  # complimentary tag and use these to restrict traffic in the firewalls.
+  name = "elm-leader"
+}
+
+resource "digitalocean_firewall" "elm-leader" {
+  name = "elm-leader"
+  tags = ["${digitalocean_tag.elm-leader.name}"]
+
+  # these are in a wacky order because the DigitalOcean API considers these a
+  # set and returns them in whatever order it likes. Terraform, on the other
+  # hand, assumes that they're as specified by the last run. So we just need to
+  # set our source order to the same implementation order of the API so we don't
+  # get weird diffs.
+  #
+  # TODO: follow https://github.com/terraform-providers/terraform-provider-digitalocean/issues/30 and upgrade when it's fixed.
+  # TODO: the rest of https://kubernetes.io/docs/setup/independent/install-kubeadm/#check-required-ports as needed
+  inbound_rule = [
+    {
+      # SSH
+      protocol         = "tcp"
+      port_range       = "22"
+      source_addresses = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      # ping
+      protocol         = "icmp"
+      port_range       = "0"
+      source_addresses = ["0.0.0.0/0", "::/0"]
+    },
+  ]
+
+  outbound_rule = [
+    {
+      protocol              = "icmp"
+      port_range            = "0"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol              = "tcp"
+      port_range            = "0"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol              = "udp"
+      port_range            = "0"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+  ]
+}
