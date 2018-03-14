155e94576cc0e618a533765bc436d13dd3b80306
Brian Hicks <brian@brianthicks.com>
2018-03-14 08:41:03 -0500

    forward Consul as well as Nomad

--- scripts/nomad-proxy.sh
+++ scripts/nomad-proxy.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PORT=4646
 HOST="$(./iac/inventory.py --host leader-0 | jq -r '.ipv4_address')"
 USER="$(./iac/inventory.py --host leader-0 | jq -r '.ansible_user')"
 KEY="$(./iac/inventory.py --host leader-0 | jq -r '.ansible_ssh_private_key_file')"
 
-echo "================================================================================="
-echo "I'm proxying port $PORT! Don't close this shell until you're done with that port."
-echo "================================================================================="
+echo "==================================="
+echo "I'm proxying cluster communication!"
+echo "==================================="
 echo
 
-ssh -L "$PORT:localhost:$PORT" -i "$KEY" "$USER@$HOST"
+ssh -L 4646:localhost:4646 -L 8500:localhost:8500 -i "$KEY" "$USER@$HOST"
